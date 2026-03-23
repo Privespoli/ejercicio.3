115d658b75e898d46f5224e9383f616b1171c9f0
Priscila Nunes <priscilavespoli92@gmail.com>
2026-03-23 15:44:02 +0100

    Start ejercicio 3

---
+++ assets/java/alumnoA.js
@@ -0,0 +1,13 @@
+// DATOS DE LA BANDA (Alumno A)
+const nombre = "Las Lokas";
+const integrantes = 2;
+const estilo = "Rock Progresivo";
+const deGira = true;
+
+// Inyectar en el DOM
+document.getElementById("nombreBanda").innerText = nombre;
+document.getElementById("numIntegrantes").innerText = integrantes;
+document.getElementById("genero").innerText = estilo;
+document.getElementById("enGira").innerText = deGira;
+
+
