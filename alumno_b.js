2a19998a67be3a4b74414b968d27205e9d5927c1
Melina <melina.dmec@gmail.com>
2026-03-23 15:44:35 +0100

    alumno b

--- alumno_b.js
+++ alumno_b.js
@@ -3,7 +3,7 @@
 // Arreglo de canciones
 const canciones = ["Code Night", "Async Love", "The Bug Blues", "Merge Conflict"];
 
-// canciones (sin ciclos)
+// canciones sin ciclos
 document.getElementById("tema1").innerText = canciones[0];
 document.getElementById("tema2").innerText = canciones[1];
 document.getElementById("tema3").innerText = canciones[2];
